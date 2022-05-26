c2ef6fee9055369180f7c322d97e40ec2212f1f1
unknown <nguyenhungthinh2504@gmail.com>
2022-05-27 00:01:05 +0700

    edit code

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SpeedConverter {
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+
+    public static void main(String[] args) {
+        long printConversion = toMilesPerHour(25.42);
+        System.out.println(printConversion);
+    }
 }
